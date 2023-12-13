f7df4f5e2656fe862686f4ae1b265a75658fa134
Alesandro Diogo <alesandromendesdiogo24@gmail.com>
2023-12-13 10:56:43 +0100

    Add project files.

---
+++ Composicao_Enumeracao/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composicao_Enumeracao.Entities
+{
+     class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Composicao_Enumeracao/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Composicao_Enumeracao.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
